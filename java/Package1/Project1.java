7e19871b094e76d95f582298b37711feb57a1b9e
sachin bhatt <dasnadas208@gmail.com>
2023-02-17 21:52:58 +0000

    sending

---
+++ java/Package1/Project1.java
@@ -0,0 +1,17 @@
+package Package1;
+
+public class Project1 {
+    //I need new house
+    public static void main(String[] args) {
+        System.out.println("I am looking house in london");
+        System.out.println("My budget 4k");
+        System.out.println("I need 3 bedrooms");
+        System.out.println("I need 2 bathrooms");
+        System.out.println("I need size 959 sq.ft");
+        System.out.println("Near school");
+        System.out.println("Near rail station");
+        System.out.println("With garden");
+        System.out.println("With furniture");
+        System.out.println("Visitors parking available");
+    }
+}
---
+++ java/Package2/Class1.java
@@ -0,0 +1,18 @@
+package Package2;
+// This is my first java program
+public class Class1 {
+    //My experience in software testing
+    public static void main(String[] args) {
+        System.out.println("Before i work in shop");
+        System.out.println("I Select software testing course");
+        System.out.println("Now i working in software testing");
+        System.out.println("I have 2 years experience");
+        System.out.println("My boss happy with my work");
+
+
+        //System.out.println("Hello i am sachin");
+        /*System.out.println("I am looking smart phone")
+        System.out.println("4 pic camera");*/
+        System.out.println("My budget £500");
+    }
+}
